f554fd63533cf05c735c8c270bf4ebadbef6cec8
Manuel Lucas <manuel.lucas@Nology.local>
2020-11-19 10:24:11 +0000

    imported routes

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Routes.module.scss";
+import { Routes } from "@reach-router";
 
 const Routes = () => {
   return (
-    <>
+    <Router>
       <p>Routes works</p>
-    </>
+    </Router>
   );
 };
 
